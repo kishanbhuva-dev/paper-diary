74245f00da5b2a711fde6083e7208d9510a5dc44
maneksatish37 <satish.manek@eviontech.com>
2025-11-06 18:47:01 +0530

    property crud updates and resource and resource type when property time make crud

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth:sanctum', 'admin']], function () {
-   Route::get('/',[App\Http\Controllers\Admin\DashboardController::class,'index']);
-   Route::apiResource('profile', App\Http\Controllers\Admin\ProfileController::class);
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+    Route::apiResource('profile', App\Http\Controllers\Admin\ProfileController::class);
 });
 Route::group(['prefix' => 'owner', 'middleware' => ['auth:sanctum', 'owner']], function () {
-   Route::get('/',[App\Http\Controllers\Owner\DashboardController::class,'index']);
-   Route::apiResource('property', App\Http\Controllers\Owner\PropertyController::class);
+    Route::get('/', [App\Http\Controllers\Owner\DashboardController::class, 'index']);
+    Route::apiResource('property', App\Http\Controllers\Owner\PropertyController::class);
 });
