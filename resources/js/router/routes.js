606a76deef3b041785e7217447b29088b3d4c645
ketanmetaliya <metaliyaketan22@gmail.com>
2025-12-03 12:20:36 +0530

    feat: redesign AuthLayout and implement AdminLayout with updated routing and footer/header components

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,66 +1,53 @@
 const routes = [
-  // 1. Default Home Route (Required for standard users)
   {
     path: "/",
-    name: "root",
     redirect: { name: "login" },
     component: () => import("../../js/layouts/AuthLayout.vue"),
-    meta: { requiresAuth: false, pageTitle: "Login" },
-  },
-
-  // 2. Auth Routes
-  {
-    name: "auth",
-    path: "/login",
-    redirect: { name: "login" },
-    component: () => import("../../js/layouts/AuthLayout.vue"),
-    meta: { authRoutes: true },
     children: [
       {
-        name: "login",
         path: "/login",
+        name: "login",
         component: () => import("../../js/pages/auth/LoginPage.vue"),
-        meta: { pageTitle: "Student Login", permission: "allowed" },
       },
       {
-        name: "register",
         path: "/register",
+        name: "register",
         component: () => import("../../js/pages/auth/RegisterPage.vue"),
-        meta: { pageTitle: "Student Register", permission: "allowed" },
       },
     ],
   },
 
-  // 3. Owner Routes
   {
-    name: "owner",
     path: "/owner",
-    redirect: { name: "owner-dashboard" },
     component: () => import("../../js/layouts/OwnerLayout.vue"),
     meta: { requiresAuth: true },
     children: [
       {
+        path: "/owner/dashboard",
         name: "owner-dashboard",
-        path: "",
         component: () => import("../../js/pages/owner/Dashboard.vue"),
         meta: { pageTitle: "Owner Dashboard" },
       },
       {
-        name: "properties",
+        name: "/owner/properties",
         path: "properties",
         component: () => import("../../js/pages/owner/Properties.vue"),
         meta: { pageTitle: "Property List" },
       },
-      // === NEW ROUTE FOR ADD/EDIT PROPERTY FORM ===
       {
-        name: "property-form",
-        // The ':id?' makes the ID parameter optional for 'Add New' functionality
+        name: "/owner/property-form",
         path: "properties-form/:id?",
         component: () => import("../../js/pages/owner/PropertiesForm.vue"),
         meta: { pageTitle: "Property Form" },
       },
-      // ===========================================
     ],
+  },
+
+  {
+    path: "/admin",
+    component: () => import("../../js/layouts/AdminLayout.vue"),
+    meta: { requiresAuth: true },
+    children: [],
   },
 ];
 
