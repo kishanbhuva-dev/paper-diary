88f117778cfd1a3d497fd7c8858e9680213df1fb
kishan bhuva <kishanbhuva@eviontech.com>
2025-12-10 16:02:25 +0530

    feat: Update Basetable to use getCellValue for improved data handling and modify Properties table column key to use a function

--- resources/js/components/global/Basetable.vue
+++ resources/js/components/global/Basetable.vue
@@ -85,7 +85,7 @@
               :key="col.key"
               class="px-6 py-3 whitespace-nowrap text-sm text-gray-700"
             >
-              {{ getNestedValue(item, col.key) }}
+              {{ getCellValue(item, col) }}
             </td>
 
             <td
@@ -264,7 +264,18 @@ const props = defineProps({
 // Solves the 'owner.name' issue by traversing objects safely
 const getNestedValue = (obj, path) => {
   if (!obj || !path) return "";
+
+  if (typeof path !== "string") return "";
+
   return path.split(".").reduce((acc, part) => acc && acc[part], obj);
+};
+
+const getCellValue = (item, col) => {
+  if (typeof col.key === "function") {
+    const rawValue = col.key(item);
+    return rawValue === null || rawValue === undefined ? "" : rawValue;
+  }
+  return getNestedValue(item, col.key);
 };
 
 // --- SEARCH & DEBOUNCE ---
